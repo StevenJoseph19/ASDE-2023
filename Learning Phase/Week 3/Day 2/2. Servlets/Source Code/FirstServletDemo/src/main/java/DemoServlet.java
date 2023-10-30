c9b6423aa2a9f1435e3cfd8b325a791920d0aa97
Stephen Samuels <stevesam1927@gmail.com>
2023-10-30 07:19:22 +0530

    week 3 day3

---
+++ Learning Phase/Week 3/Day 2/2. Servlets/Source Code/FirstServletDemo/src/main/java/DemoServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.http.*;
+import javax.servlet.*;
+import java.io.*;
+public class DemoServlet extends HttpServlet{
+    public void doGet(HttpServletRequest req,HttpServletResponse res)
+            throws ServletException,IOException
+    {
+        res.setContentType("text/html");//setting the content type
+        PrintWriter pw=res.getWriter();//get the stream to write the data
+
+//writing html in the stream  
+        pw.println("<html><body>");
+        pw.println("Welcome to servlet demo");
+        pw.println("</body></html>");
+
+        pw.close();//closing the stream
+    }}
